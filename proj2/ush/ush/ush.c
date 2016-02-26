6b39690d6106f76d6574cf707cb53dc75bfa1dbb
arjunaugustine <arjunaugustine@gmail.com>
2016-02-25 23:23:38 -0500

    Almost done

--- proj2/ush/ush/ush.c
+++ proj2/ush/ush/ush.c
@@ -13,266 +13,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "parse.h"
-
-//#define DEBUG_MODE
-#ifdef DEBUG_MODE
-    #define DETAIL(x) \
-        printf(#x": %d\n", x);
-    #define D(x) \
-        printf(x);
-#else
-    #define DETAIL(x)
-    #define D(x)
-#endif
-
-extern char **environ;
-
-int  status = 1;
-
-void checkNextCmd (Cmd C, int pipeFD[2]);
-
-void checkDescriptor (int descriptor) {
-    if (descriptor == -1) {
-        perror("Unable to Open File");
-        _exit(EXIT_FAILURE);
-    }
-}
-
-void redirectInput (int descriptor) {
-    dup2(descriptor, STDIN_FILENO);
-    close(descriptor);
-}
-
-void redirectOutput (int descriptor, bool errorOut) {
-    dup2(descriptor, STDOUT_FILENO);
-    if (errorOut) {
-        dup2(descriptor, STDERR_FILENO);
-    }
-    close(descriptor);
-}
-
-void openFileForRead (char *infile) {
-    int descriptor = 0;
-    descriptor = open(infile,(O_RDONLY),(S_IRUSR|S_IRGRP|S_IROTH));
-    checkDescriptor(descriptor);
-    redirectInput(descriptor);
-}
-
-void openFileForWrite (char *outfile, bool errorOut) {
-    int descriptor = 0;
-    descriptor = open(outfile,(O_CREAT|O_WRONLY|O_TRUNC),(S_IRWXU|S_IRWXG|S_IRWXO));
-    checkDescriptor(descriptor);
-    redirectOutput(descriptor, errorOut);
-}
-
-void openFileForAppend (char *outfile, bool errorOut) {
-    int descriptor = 0;
-    descriptor = open(outfile,(O_APPEND|O_WRONLY),(S_IRWXU|S_IRWXG|S_IRWXO));
-    checkDescriptor(descriptor);
-    redirectOutput(descriptor, errorOut);
-}
-
-void waitOnPid (pid_t pid) {
-    int waitStatus;
-    pid_t wpid;
-    do {
-        wpid = waitpid(pid, &waitStatus, WUNTRACED);
-    } while (!WIFEXITED(waitStatus) && !WIFSIGNALED(waitStatus));
-}
-
-void createPipe(int pipeFD[2]) {
-    pipe(pipeFD);
-    if (pipeFD < 0) {
-        perror("Could not create pipe");
-        _exit(EXIT_FAILURE);
-    }
-}
-
-void writeToPipe (int pipeFD[2], bool errorOut) {
-    close(pipeFD[0]); // Close unused read end
-    dup2(pipeFD[1], STDOUT_FILENO);
-    if (errorOut) {
-        dup2(pipeFD[1], STDERR_FILENO);
-    }
-    close(pipeFD[1]);
-}
-
-int  isBuiltin (char *C) {
-    if (!strcmp(C, "cd")) {
-        return 1;
-    }
-    if (!strcmp(C, "echo")) {
-        return 2;
-    }
-    if (!strcmp(C, "logout")) {
-        return 3;
-    }
-    if (!strcmp(C, "nice")) {
-        return 4;
-    }
-    if (!strcmp(C, "pwd")) {
-        return 5;
-    }
-    if (!strcmp(C, "setenv")) {
-        return 6;
-    }
-    if (!strcmp(C, "unsetenv")) {
-        return 7;
-    }
-    if (!strcmp(C, "where")) {
-        return 8;
-    }
-    return 0;
-}
-
-void ushExCd(Cmd C){
-    int returnVal = 1;
-    char *env    = (char*)malloc(100*sizeof(char));
-    env = ((C->nargs == 1) ? getenv("HOME") : C->args[1]);
-    returnVal = chdir(env);
-    if(-1 == returnVal){
-        perror("cd");
-    }
-}
-
-void ushExEcho (Cmd C) {
-    char *env    = (char*)malloc(100*sizeof(char));
-    if (C->args[1] == NULL) {
-        fprintf(stdout, "\n");
-    } else {
-        int i = 1;
-        while (C->args[i] != NULL) {
-            if (!strncmp((C->args[i]), "$", 1)) {
-                env = getenv((C->args[i])+1);
-                if (env != NULL) {
-                    fprintf(stdout, "%s ",env);
-                }
-            } else {
-                fprintf(stdout, "%s ",C->args[i]);
-            }
-            i++;
-        }
-        fprintf(stdout, "\n");
-    }
-}
-
-void ushExNice (Cmd C) {
-    pid_t pid = getpid();
-    if (C->nargs == 1) {
-        if (-1 == setpriority(PRIO_PROCESS, pid, 4)) {
-            perror("setpriority");
-        }
-    } else if (C->nargs == 2) {
-        if (-1 == setpriority(PRIO_PROCESS, pid, atoi(C->args[1]))) {
-            perror("setpriority");
-        }
-    } else if (C->nargs == 3) {
-        // Implement
-    } else {
-        fprintf(stderr, "nice: Too many arguments.\n");
-    }
-}
-
-void ushExPwd (Cmd C) {
-    char *env    = (char*)malloc(100*sizeof(char));
-    env = getcwd(env, 256);
-    if (env == NULL) {
-        perror("PWD not set");
-    } else {
-        fprintf(stdout, "%s\n", env);
-    }
-}
-
-void ushExSetenv (Cmd C) {
-    char *env    = (char*)malloc(100*sizeof(char));
-    char *envVal = (char*)malloc(100*sizeof(char));
-    int returnVal;
-    if (C->args[1] == NULL) {
-        int iter = 0;
-        env = environ[0];
-        while (env != NULL) {
-//            envVal = getenv(env);
-//            if (envVal == NULL) {
-//                perror("setEnv");
-//                break;
-//            }
-            fprintf(stdout, "%s=%s\n",env, envVal);
-            env = environ[++iter];
-        }
-    } else if (C->args[2] == NULL) {
-        returnVal = setenv(C->args[1], "", 1);
-        if (returnVal == -1) {
-            perror("setenv");
-        }
-    } else if (C->args[3] == NULL) {
-        returnVal = setenv(C->args[1], C->args[2], 1);
-        if (returnVal == -1) {
-            perror("setenv");
-        }
-    } else {
-        fprintf(stderr, "setenv: Too many arguments.\n");
-    }
-    free(env);
-    free(envVal);
-}
-
-void ushExUnsetenv (Cmd C) {
-    int returnVal;
-    if (C->args[1] == NULL) {
-        fprintf(stdout, "unsetenv: Too few arguments.\n");
-    } else if (C->nargs > 2) {
-        fprintf(stdout, "unsetenv: Too many arguments.\n");
-    } else {
-        returnVal = unsetenv(C->args[1]);
-        if (returnVal == -1) {
-            perror("unsetenv");
-        }
-    }
-}
-
-void ushExWhere (Cmd C) {
-    // All checked except occassional unwanted output.
-    char *env = (char*)malloc(500*sizeof(char));
-    if (C->nargs == 1) {
-        fprintf(stderr, "where: Too few arguments.\n");
-    }
-    int iter = 1;
-    char *arg = C->args[iter];
-    while (arg != NULL) {
-        if (isBuiltin(arg)) {
-            fprintf(stdout, "%s: shell built-in command\n",arg);
-        }
-        char *pathCopy = (char*)malloc(900*sizeof(char));
-        strcpy(pathCopy, getenv("PATH"));
-        while ((strcpy(env, strsep(&pathCopy, ":")))) {
-            strcat(env,"/");
-            strcat(env,arg);
-            if( access( env, F_OK ) != -1 ) {
-                fprintf(stdout, "%s\n",env);
-                break;
-            }
-            if (!pathCopy) {
-                break;
-            }
-        }
-        strcpy(env, getenv("PWD"));
-        strcat(env,"/");
-        strcat(env,arg);
-        if( access( env, F_OK ) != -1 ) {
-            fprintf(stdout, "%s\n",env);
-        }
-        iter++;
-        arg = C->args[iter];
-        free(pathCopy);
-    }
-    free(env);
-}
+#include "ush.h"
 
 void ushExBuiltIn (Cmd C, int builtInID) {
     DETAIL(builtInID);
-    int fildesout = dup(STDOUT_FILENO);
-    int fildeserr = dup(STDERR_FILENO);
-    int fildesinp = dup( STDIN_FILENO);
+    int fildesout, fildesinp, fildeserr;
+    saveDescriptors(&fildesout, &fildesinp, &fildeserr);
     if (C->in == Tin) {
         openFileForRead(C->infile);
     }
@@ -291,7 +37,7 @@ void ushExBuiltIn (Cmd C, int builtInID) {
         case TappErr:
             openFileForAppend(C->outfile, true);
             break;
-        default:
+        default:                    // BuiltIn commands are not pipes
             fprintf(stderr, "Unknown Command Out Token\n");
             _exit(EXIT_FAILURE);
     }
@@ -325,9 +71,7 @@ void ushExBuiltIn (Cmd C, int builtInID) {
             fprintf(stderr, "inbuiltCmd Switch default Error!");
             break;
     }
-    dup2(fildesinp,  STDIN_FILENO);
-    dup2(fildesout, STDOUT_FILENO);
-    dup2(fildeserr, STDERR_FILENO);
+    restoreDescriptors(fildesout, fildesinp, fildeserr);
 }
 
 void ushExCmd (Cmd C, int pipeFD[2]) {
@@ -365,53 +109,51 @@ void ushExCmd (Cmd C, int pipeFD[2]) {
             _exit(EXIT_FAILURE);
     }
     if (execvp(C->args[0], C->args) == -1) {
-        perror("Error");
+        perror("Error");    // This part of code never executes if execvp is success
     }
 }
 
 void pipeIn (int pipeInFD[2], Cmd C) {
     D("pipeIn\n");
-    pid_t pid;
-    close(pipeInFD[1]); // Close unused write end
-    int builtIn = isBuiltin(C->args[0]);
+    close(pipeInFD[1]);                     // Close unused write end
+    int builtIn = isBuiltin(C->args[0]);    // Check last command for BuiltIn
     if (builtIn > 0) {
         ushExBuiltIn(C, builtIn);
         return;
     }
-    pid = fork();
+    pid_t pid = fork();
     if (pid < 0) {
         perror("Fork Error");
-    } else if (pid == 0) { // Child Process
+    } else if (pid == 0) {      // Child Process
         D("pipeInChild\n");
         dup2(pipeInFD[0], STDIN_FILENO);
         close(pipeInFD[0]);
         ushExCmd(C, pipeInFD);
         _exit(EXIT_FAILURE);
-    } else {
+    } else {                    // Parent Process
         D("pipeInParent\n");
         close(pipeInFD[0]);
-        waitOnPid(pid);
+        waitOnPid(pid);         // Wait on child an exit as this is the last cmd
     }
 }
 
 void pipeInOut (int pipeInFD[2], Cmd C) {
-    D("pipeInOut\n");
-    pid_t pid;
+    D("pipeInOut\n");           // C is pipe in middle of other commands
     int pipeOutFD[2];
-    createPipe(pipeOutFD);    // Create an Output Pipe
-    close(pipeInFD[1]); // Close unused write end of Input Pipe
-    pid = fork();
+    createPipe(pipeOutFD);      // Create an Output Pipe
+    close(pipeInFD[1]);         // Close unused write end of Input Pipe
+    pid_t pid = fork();
     if (pid < 0) {
         perror("Fork Error");
-    } else if (pid == 0) { // Child Process
+    } else if (pid == 0) {                  // Child Process
         D("ushInOutChild\n");
-        dup2( pipeInFD[0],  STDIN_FILENO);
-        dup2(pipeOutFD[1], STDOUT_FILENO);
+        dup2( pipeInFD[0],  STDIN_FILENO);  // Read from Input Pipe
+        dup2(pipeOutFD[1], STDOUT_FILENO);  // Write to Output Pipe
         close( pipeInFD[0]);
         close(pipeOutFD[1]);
         ushExCmd(C, pipeInFD);
         _exit(EXIT_FAILURE);
-    } else {
+    } else {                                // Parent Process
         close(pipeInFD[0]);
         D("ushInOutParent\n");
         waitOnPid(pid);
@@ -419,56 +161,45 @@ void pipeInOut (int pipeInFD[2], Cmd C) {
     }
 }
 
-void checkNextCmd (Cmd C, int pipeFD[2]) {
+void checkNextCmd (Cmd C, int pipeFD[2]) {  // C is next command to be evaluated
     if (C != NULL) {
-        if (C->next != NULL) {
+        if (C->next != NULL) {              // Then C is a pipe command
             pipeInOut(pipeFD, C);
-        } else {
+        } else {                            // Else C is the last one in the statement
             pipeIn(pipeFD, C);
         }
     }
 }
 
 void ushExStmt (Pipe P) {
-    pid_t pid;
     int pipeFD[2];
-    Cmd cmd1 = P->head;
-    if (cmd1->out != Tpipe) {
+    Cmd cmd1 = P->head;                     // First command in pipe
+    if (cmd1->next == NULL) {               // If command is not a pipe,
         int builtIn = isBuiltin(cmd1->args[0]);
-        DETAIL(builtIn);
-        if (builtIn > 0) {
-            ushExBuiltIn(cmd1, builtIn);
-            return;
+        if (builtIn > 0) {                  // and is builtIn
+            ushExBuiltIn(cmd1, builtIn);    // Execute BuiltIn commands
+            return;                         // Return to parse more pipes
         }
-    } else {
+    } else {                                // If first command is a pipe
         createPipe(pipeFD);
     }
-    pid = fork();
+    pid_t pid = fork();                     // If cmd is not BuiltIn, Fork
     if (pid < 0) {
         perror("Fork Error");
-    } else if (pid == 0) { // Child Process
+    } else if (pid == 0) {                  // Child Process
         D("ushExStmtChild\n");
         ushExCmd(cmd1, pipeFD);
         _exit(EXIT_FAILURE);
-    } else { // Parent process
+    } else {                                // Parent process
         D("ushExStmtParent\n");
-        int waitStatus;
-        pid_t wpid;
-        do {
-            wpid = waitpid(pid, &waitStatus, WUNTRACED);
-//            if (-1 == wpid) {
-//                perror("negative wpid");
-//                _exit(EXIT_FAILURE);
-//            }
-        } while (!WIFEXITED(waitStatus) && !WIFSIGNALED(waitStatus));
-        D("ushExStmtParentWaitOver.\n");
+        waitOnPid(pid);
         checkNextCmd(cmd1->next, pipeFD);
     }
 }
 
 void ushExLine (Pipe P) {
-    while (P != NULL) {
-        ushExStmt(P);
+    while (P != NULL) {                         // Iterate over all instructions in pipe
+        ushExStmt(P);                           // To execute them all one by one.
         P = P->next;
     }
 }
@@ -476,16 +207,42 @@ void ushExLine (Pipe P) {
 int  main(int argc, const char * argv[]) {
     Pipe P;
     char host[256];
-    if(gethostname(host, sizeof(host)) < 0) {
+    if(gethostname(host, sizeof(host)) < 0) {       // get hostname for prompt
         D("Error: Couldnt get hostname! Aborting...\n");
         exit (-1);
     }
-    do {
-        printf("%s$ ",host);
-        P = parse();
-        if (!strcmp(P->head->args[0], "end") && !isatty(fileno(STDIN_FILENO))){
+    char * ushrc = (char *)malloc(500*sizeof(char));
+    strcpy(ushrc, getenv("HOME"));
+    strcat(ushrc, "/.ushrc");
+    if( access( ushrc, F_OK ) != -1 ) {           // If .ushrc file exists in HOME:
+        int fildesout, fildesinp, fildeserr;
+        saveDescriptors(&fildesout, &fildesinp, &fildeserr);
+        openFileForRead(ushrc);
+        do {
+            P = parse();
+            if (!strcmp(P->head->args[0], "end")) {
+                freePipe(P);
+                break;
+            }
+            ushExLine(P);
             freePipe(P);
-            exit(EXIT_SUCCESS);
+        } while (1);
+        restoreDescriptors(fildesout, fildesinp, fildeserr);
+    }
+    do {
+        if (isatty(STDIN_FILENO)) {
+            printf("%s$ ",host);
+            fflush(stdout);            
+        }
+        P = parse();
+        if (P != NULL) {
+            if (!strcmp(P->head->args[0], "end")){
+                if (isatty(STDIN_FILENO)) {
+                    printf("\n");
+                }
+                freePipe(P);
+                exit(EXIT_SUCCESS);
+            }
         }
         ushExLine(P);
         freePipe(P);
---
+++ proj2/ush/ush/ush.h
@@ -0,0 +1,282 @@
+//
+//  ush.h
+//  ush
+//
+//  Created by Arjun Augustine on 2/25/16.
+//  Copyright © 2016 Arjun Augustine. All rights reserved.
+//
+
+#ifndef ush_h
+#define ush_h
+
+//#define DEBUG_MODE
+#ifdef DEBUG_MODE
+#define DETAIL(x) \
+printf(#x": %d\n", x);
+#define D(x) \
+printf(x);
+#else
+#define DETAIL(x)
+#define D(x)
+#endif
+
+int status = 1;
+
+extern char **environ;
+
+void checkNextCmd (Cmd C, int pipeFD[2]);
+
+void checkDescriptor (int descriptor) {
+    if (descriptor == -1) {
+        perror("Unable to Open File");
+        _exit(EXIT_FAILURE);
+    }
+}
+
+void redirectInput (int descriptor) {
+    dup2(descriptor, STDIN_FILENO);
+    close(descriptor);
+}
+
+void redirectOutput (int descriptor, bool errorOut) {
+    dup2(descriptor, STDOUT_FILENO);
+    if (errorOut) {
+        dup2(descriptor, STDERR_FILENO);
+    }
+    close(descriptor);
+}
+
+void openFileForRead (char *infile) {
+    int descriptor = 0;
+    descriptor = open(infile,(O_RDONLY),(S_IRUSR|S_IRGRP|S_IROTH));
+    checkDescriptor(descriptor);
+    redirectInput(descriptor);
+}
+
+void openFileForWrite (char *outfile, bool errorOut) {
+    int descriptor = 0;
+    descriptor = open(outfile,(O_CREAT|O_WRONLY|O_TRUNC),(S_IRWXU|S_IRWXG|S_IRWXO));
+    checkDescriptor(descriptor);
+    redirectOutput(descriptor, errorOut);
+}
+
+void openFileForAppend (char *outfile, bool errorOut) {
+    int descriptor = 0;
+    descriptor = open(outfile,(O_APPEND|O_WRONLY),(S_IRWXU|S_IRWXG|S_IRWXO));
+    checkDescriptor(descriptor);
+    redirectOutput(descriptor, errorOut);
+}
+
+void restoreDescriptors (int fildesout, int fildesinp, int fildeserr) {
+    dup2(fildesinp,  STDIN_FILENO);             // Restore the descriptors
+    dup2(fildesout, STDOUT_FILENO);             // to previous values.
+    dup2(fildeserr, STDERR_FILENO);
+}
+
+void saveDescriptors (int *fildesout, int *fildesinp, int *fildeserr) {
+    *fildesout = dup(STDOUT_FILENO);            // Duplicate the current STDOUT,
+    *fildeserr = dup(STDERR_FILENO);            // STDERR, STDIN descriptors to
+    *fildesinp = dup( STDIN_FILENO);            // restore them after indirection
+}
+
+void waitOnPid (pid_t pid) {
+    int waitStatus;
+    do {
+        pid_t wpid = waitpid(pid, &waitStatus, WUNTRACED);
+        if (-1 == wpid) {
+            //                perror("negative wpid");
+            //                _exit(EXIT_FAILURE);
+        }
+    } while (!WIFEXITED(waitStatus) && !WIFSIGNALED(waitStatus));
+}
+
+void createPipe(int pipeFD[2]) {
+    pipe(pipeFD);
+    if (pipeFD < 0) {
+        perror("Could not create pipe");
+        _exit(EXIT_FAILURE);
+    }
+}
+
+void writeToPipe (int pipeFD[2], bool errorOut) {
+    close(pipeFD[0]); // Close unused read end
+    dup2(pipeFD[1], STDOUT_FILENO);
+    if (errorOut) {
+        dup2(pipeFD[1], STDERR_FILENO);
+    }
+    close(pipeFD[1]);
+}
+
+int  isBuiltin (char *C) {
+    if (!strcmp(C, "cd")) {
+        return 1;
+    }
+    if (!strcmp(C, "echo")) {
+        return 2;
+    }
+    if (!strcmp(C, "logout")) {
+        return 3;
+    }
+    if (!strcmp(C, "nice")) {
+        return 4;
+    }
+    if (!strcmp(C, "pwd")) {
+        return 5;
+    }
+    if (!strcmp(C, "setenv")) {
+        return 6;
+    }
+    if (!strcmp(C, "unsetenv")) {
+        return 7;
+    }
+    if (!strcmp(C, "where")) {
+        return 8;
+    }
+    return 0;
+}
+
+void ushExCd(Cmd C){
+    char *env = ( (C->nargs == 1)          ? getenv("HOME")   :
+                 (!strcmp(C->args[1], "-")) ? getenv("OLDPWD") : C->args[1]);
+    if (env == NULL) {
+        if (getenv("HOME") == NULL) {
+            env = "cd: HOME not set\n";
+        } else {
+            env = "cd: OLDPWD not set\n";
+        }
+        fprintf(stdout, "%s", env);
+        return;
+    }
+    int returnVal = chdir(env);
+    if(-1 == returnVal){
+        perror("cd");
+    }
+}
+
+void ushExEcho (Cmd C) {
+    if (C->args[1] == NULL) {
+        fprintf(stdout, "\n");
+    } else {
+        int i = 1;
+        while (C->args[i] != NULL) {
+            if (!strncmp((C->args[i]), "$", 1)) {
+                char *env    = (char*)malloc(500*sizeof(char));
+                env = getenv((C->args[i])+1);
+                if (env != NULL) {
+                    fprintf(stdout, "%s ",env);
+                }
+                free(env);
+            } else {
+                fprintf(stdout, "%s ",C->args[i]);
+            }
+            i++;
+        }
+        fprintf(stdout, "\n");
+    }
+}
+
+bool isADigit (char c) {
+    if (c == '+' || c == '-' || (c >= '0' && c <= '9') ) {
+        return true;
+    }
+    return false;
+}
+
+void ushExNice (Cmd C) {
+    pid_t pid = getpid();
+    if (C->nargs == 1) {
+        if (-1 == setpriority(PRIO_PROCESS, pid, 4)) {
+            perror("setpriority");
+        }
+    } else if (C->nargs == 2 && isADigit(*(C->args[1]))) {
+        if (-1 == setpriority(PRIO_PROCESS, pid, atoi(C->args[1]))) {
+            perror("setpriority");
+        }
+    } else if (C->nargs == 3 || !isADigit(*(C->args[1])) ) {
+        // Implement
+    } else {
+        fprintf(stderr, "nice: Too many arguments.\n");
+    }
+}
+
+void ushExPwd (Cmd C) {
+    char *env    = (char*)malloc(500*sizeof(char));
+    env = getcwd(env, 256);
+    if (env == NULL) {
+        perror("PWD not set");
+    } else {
+        fprintf(stdout, "%s\n", env);
+    }
+    free(env);
+}
+
+void ushExSetenv (Cmd C) {
+    int returnVal = 0;
+    if (C->nargs == 1) { // Print all lines in environ
+        char **env = environ;
+        while (*env != NULL) {
+            fprintf(stdout, "%s\n",*env);
+            env++;
+        }
+    } else if (C->nargs == 2) { // Set environ variable to null string
+        returnVal = setenv(C->args[1], "", 1);
+    } else if (C->nargs == 3) { // Set environ variable to specified arg
+        returnVal = setenv(C->args[1], C->args[2], 1);
+    } else {                    // Too many arguments
+        fprintf(stderr, "setenv: Too many arguments.\n");
+    }
+    if (returnVal == -1) {      // In case the setenv-s fail.
+        perror("setenv");
+    }
+}
+
+void ushExUnsetenv (Cmd C) {
+    if (C->nargs < 2) {         // Too few  arguments
+        fprintf(stdout, "unsetenv: Too few  arguments.\n");
+    } else if (C->nargs > 2) {  // Too many arguments
+        fprintf(stdout, "unsetenv: Too many arguments.\n");
+    } else {                    // Go ahead and unset
+        if (-1 == unsetenv(C->args[1])) {
+            perror("unsetenv");
+        }
+    }
+}
+
+void ushExWhere (Cmd C) {
+    // All checked except occassional unwanted output.
+    // Last check in CWD/PWD???
+    char *env;
+    if (C->nargs == 1) {
+        fprintf(stderr, "where: Too few arguments.\n");
+    }
+    int iter = 1;
+    char *arg = C->args[iter];
+    while (arg != NULL) {   // Iterate all arguments of where command
+        if (isBuiltin(arg)) {
+            fprintf(stdout, "%s: shell built-in command\n",arg);
+        }
+        char *pathCopy = strdup(getenv("PATH"));
+        while ((env = strdup(strsep(&pathCopy, ":")) )) { // Choose absolute paths in $Path
+            strcat(env,"/");                            // One by one and concatenate
+            strcat(env,arg);                            // Them with /<arg>
+            if( access( env, F_OK ) != -1 ) {           // If this file exists, print
+                fprintf(stdout, "%s\n",env);
+                break;
+            }
+            if (!pathCopy) {
+                break;
+            }
+        }
+        strcpy(env, getenv("PWD"));                     // Check in current directory also
+        strcat(env,"/");
+        strcat(env,arg);
+        if( access( env, F_OK ) != -1 ) {
+            fprintf(stdout, "%s\n",env);
+        }
+        arg = C->args[++iter];
+        free(pathCopy);
+        free(env);
+    }
+}
+
+#endif /* ush_h */
